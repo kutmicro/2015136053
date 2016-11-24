a64c2ba7a98793318afdc633f7e51cac6ec1ee4f
2013136082_R-pet'sSafeHabitat <kutmicro@gmail.com>
2016-11-24 17:44:13 +0900

    Add files via upload

---
+++ 진동 모터 모듈 - Ywrobot vibration motor/VibrationMotorModule/VibrationMotorModule.ino
@@ -0,0 +1,16 @@
+// Code to vibrate the motor every minute
+ const int motorPin = 37;
+
+ void setup()
+ {
+ pinMode(motorPin, OUTPUT);
+ }
+
+ void loop()
+ {
+ digitalWrite(motorPin, HIGH);
+ delay(1000);
+ digitalWrite(motorPin, LOW);
+ delay(59000);
+ }
+
